d17cb1dcc6f69fee1064da2477a5041c4a5b57c5
Pratik Bhaliya <ppmit30@gmail.com>
2019-08-29 07:33:32 +1000

    Initial Commit

---
+++ SunLight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SunLight
+//
+//  Created by Pratik Bhaliya on 29/8/19.
+//  Copyright © 2019 Pratik Bhaliya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SunLightTests/SunLightTests.swift
@@ -0,0 +1,34 @@
+//
+//  SunLightTests.swift
+//  SunLightTests
+//
+//  Created by Pratik Bhaliya on 29/8/19.
+//  Copyright © 2019 Pratik Bhaliya. All rights reserved.
+//
+
+import XCTest
+@testable import SunLight
+
+class SunLightTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
